e298a58169d145393dfd4296d028b8fba63fc33d
Jet Pham <55770902+jetpham@users.noreply.github.com>
2024-11-09 19:18:41 -0800

    fix remove unused imports

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -3,11 +3,6 @@ import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
 import {
     Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
 } from "@/components/ui/card"
 
 const ControlPanel = () => {
