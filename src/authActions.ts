4014ca48053af6e04fd30729884699a1ae455f42
Jet Pham <55770902+jetpham@users.noreply.github.com>
2024-11-10 09:01:34 -0800

    feat: add signout button and avatar meow

--- src/authActions.ts
+++ src/authActions.ts
@@ -1,7 +1,10 @@
 'use server';
 
-import { signIn } from "@/auth";
+import { signIn, signOut } from "@/auth";
 
 export async function handleSignIn() {
     await signIn("google");
+}
+export async function handleSignOut() {
+    await signOut();
 }
--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 import {
     Card,
 } from "@/components/ui/card"
 import SubmitMarker from './SubmitMarker';
 import './ControlPanel.css';
-import { handleSignIn } from "@/authActions"
+import { handleSignIn, handleSignOut } from "@/authActions"
+import { auth } from "@/auth"
 
-const ControlPanel = () => {
+
+const ControlPanel = async () => {
+    const session = await auth()
+
+    const getInitials = (name: string) => {
+        const names = name.split(' ');
+        const initials = names.map(n => n[0]).join('');
+        return initials.toUpperCase();
+    };
+
     return (
         <Card className='ControlPanel'>
-            <form
-                action={handleSignIn}
-            >
-                <Button variant='outline' className='SignIn' type='submit'>Sign In</Button>
-            </form>
-            <Textarea className='TextArea' />
-            <Button variant='outline' className='Submit'>Send</Button>
-            <SubmitMarker />
+            {session?.user ? (
+                <>
+                    <form action={handleSignOut} className='SignOutForm'>
+                        <Avatar className='Avatar'>
+                            <AvatarImage src={session.user.image ?? ''} />
+                            <AvatarFallback>{getInitials(session.user.name ?? '')}</AvatarFallback>
+                        </Avatar>
+                        <Button variant='destructive' className='SignOut' type='submit'>Sign Out</Button>
+                    </form>
+                    <Textarea className='TextArea' />
+                    <Button variant='outline' className='Submit'>Send</Button>
+                    <SubmitMarker />
+                </>
+            ) : (
+                <form action={handleSignIn}>
+                    <Button variant='outline' className='SignIn' type='submit'>Sign In</Button>
+                </form>
+            )}
         </Card>
     );
 };
