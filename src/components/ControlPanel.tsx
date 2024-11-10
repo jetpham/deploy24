8365924612f0f78df391476703267d257a050fca
Jet Pham <55770902+jetpham@users.noreply.github.com>
2024-11-10 00:57:05 -0800

    feat: Add Oauth

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -4,13 +4,21 @@ import { Button } from "@/components/ui/button"
 import {
     Card,
 } from "@/components/ui/card"
+import SubmitMarker from './SubmitMarker';
 import './ControlPanel.css';
+import { handleSignIn } from "@/authActions"
 
 const ControlPanel = () => {
     return (
         <Card className='ControlPanel'>
+            <form
+                action={handleSignIn}
+            >
+                <Button variant='outline' className='SignIn' type='submit'>Sign In</Button>
+            </form>
             <Textarea className='TextArea' />
             <Button variant='outline' className='Submit'>Send</Button>
+            <SubmitMarker />
         </Card>
     );
 };
